ccc4928dca895370a6594b02eef744d0b2985214
benkbh03 <benkbh03@gmail.com>
2026-03-16 21:52:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
    CYKELBØRSEN – main.js
    ============================================================ */
 
+
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const SUPABASE_URL = 'https://ktufgncydxhkhfttojkh.supabase.co';
